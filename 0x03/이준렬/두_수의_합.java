489491d0c0eb12e1047a1df9a98b043e6d93e3b5
lee-junr <wns2610@icloud.com>
2024-05-20 23:41:57 +0900

    add : 240520 boj_3273 Solved

--- 0x03/이준렬/두_수의_합.java
+++ 0x03/이준렬/두_수의_합.java
@@ -2,35 +2,44 @@
 // 시간 제한이 1초이고 배열의 개수가 1_000_000개 -> 시간복잡도가 O(NlogN) 보다 빨라야함.
 // 이중 for문으로 모든 경우의 수를 따지면 O(N^2) 으로 통과 못함.
 // 1. 자연수 x가 주어지면 a_i 를 빼서 a_j가 있는지 체크 -> HashSet 을 사용하면 시간복잡도가 O(1)
+// 2. 투포인터 사용
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.Arrays;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     // Input
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
+    int n = Integer.parseInt(br.readLine()); // n
+    //    number arr
     String[] strArr = br.readLine().split(" ");
-    int x = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(strArr[i]);
+    }
+    int x = Integer.parseInt(br.readLine()); // x
     br.close();
 
-    // Initialize variables
-    Set<Integer> set = new HashSet<>();
+    // 투포인터 사용.
+    Arrays.sort(arr); // 투포인터는 sorting 필수.
+    int l = 0;
+    int r = n - 1;
     int count = 0;
-
-    for (int index = 0; index < n; index++) {
-      int i = Integer.parseInt(strArr[index]);
-      int j = x - i;
-      if (set.contains(j)) {
+    while (l < r) {
+      int sum = arr[l] + arr[r];
+      if (sum == x) {
         count++;
+        l++;
+        r--;
+      } else if (sum < x) {
+        l++;
+      } else {
+        r--;
       }
-      set.add(i);
     }
 
     // Output
