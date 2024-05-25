b7c395a0ddb835980ce4b4c64b482c2c32abbae0
lee-junr <wns2610@icloud.com>
2024-05-25 14:37:01 +0900

    add : 240525 boj_5397 solved

---
+++ 0x04/이준렬/키로거.java
@@ -0,0 +1,58 @@
+// 키로거_5397
+// 풀이
+// cursor 설정해서 해당 위치에 저장.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    // TC 분리
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine()); // TC개수
+
+    // 각 TC 당 하나씩 처리.
+    for (int test_case = 1; test_case <= T; test_case++) {
+      // Input
+      LinkedList<Character> keyLogList = new LinkedList<>();
+      String keyLog = br.readLine();
+      ListIterator<Character> cursor = keyLogList.listIterator();
+
+      for (char c : keyLog.toCharArray()) {
+        switch (c) {
+          case '>':
+            if (cursor.hasNext()) {
+              cursor.next();
+            }
+            break;
+          case '<':
+            if (cursor.hasPrevious()) {
+              cursor.previous();
+            }
+            break;
+          case '-':
+            if (cursor.hasPrevious()) {
+              cursor.previous();
+              cursor.remove();
+            }
+            break;
+          default:
+            cursor.add(c);
+            break;
+        }
+      }
+
+      // Output
+      StringBuilder sb = new StringBuilder();
+      for (char c : keyLogList) {
+        sb.append(c);
+      }
+      System.out.println(sb.toString());
+    }
+    br.close();
+  }
+}
